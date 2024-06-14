878a3567aa4fb95a6b29ceec443cb39223fef635
A-y-A-n-616 <chromatic241@gmail.com>
2024-06-14 17:48:32 +0530

    en

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ def run_terminal_commands():
         # Run terminal commands
         subprocess.run(["pip","install","-r","requirements.txt"])
         subprocess.run(["python","-m", "spacy","download","en_core_web_sm"])
+        subprocess.run(["python","-m","spacy","download","en"])
     except Exception as e:
         print(f"Error occurred: {e}")
 run_terminal_commands()
